5999698dae0156bb45111a5b92dd4bf3a24aed8b
hudson <hfinn@autodist.com>
2024-01-04 11:06:33 -0500

    Kids with candies

--- src/kids_with_candies.rs
+++ src/kids_with_candies.rs
@@ -1,3 +1,13 @@
 pub fn solutiion(candies: Vec<i32>, extra_candies: i32) -> Vec<bool> {
-    vec![true]
+    let mut result: Vec<bool> = Vec::new();
+
+    for kid in &candies {
+        if kid + extra_candies >= *candies.iter().max().unwrap() {
+            result.push(true);
+        } else {
+            result.push(false);
+        }
+    }
+
+    result
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,5 @@
-fn main() {}
+mod kids_with_candies;
+
+fn main() {
+    println!("{:?}", kids_with_candies::solutiion(vec![2, 3, 5, 1, 3], 3));
+}
