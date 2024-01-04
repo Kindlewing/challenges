12df5214f90453024d03d4ffa68cc49a6d606d1c
hudson <hfinn@autodist.com>
2024-01-03 20:28:45 -0500

    Working

--- src/merge_strings_alternate.rs
+++ src/merge_strings_alternate.rs
@@ -1,5 +1,9 @@
 pub fn solution(word1: String, word2: String) -> String {
     let mut merged = String::new();
+    let p1: i32 = 0;
+    let p2: i32 = 0;
+
+    while p1 < word1.len() || p2 < word2.len() {}
 
     merged
 }
